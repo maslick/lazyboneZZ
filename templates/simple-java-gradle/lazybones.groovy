6638be2c4d5550059fe53551eaf89d68796a6cc0
Pavel Maslov <pavel.maslov@cloud.si>
2016-06-11 19:26:44 +0200

    some bugs fixed

--- templates/simple-java-gradle/lazybones.groovy
+++ templates/simple-java-gradle/lazybones.groovy
@@ -1,7 +1,8 @@
 def props = [:]
-props.version = ask("Define value for 'project_name' [project]: ", "project", "project_name")
-props.group = ask("Define value for 'group' [com.maslick.{helloworld]: ", "helloworld", "group")
+props.project = ask("Define value for 'project_name' [project]: ", "project", "project")
+props.group = ask("Define value for 'group' [com.maslick.{helloworld}]: ", "helloworld", "group")
 props.version = ask("Define value for 'version' [0.1]: ", "0.1", "version")
 
 
 processTemplates "build.gradle", props
+processTemplates "settings.gradle", props
